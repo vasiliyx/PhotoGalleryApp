41a897ed62c5fc9461484e90fa5a51369ec796bb
Vasiliy Baryshnikov <vasiliy.baryshnikov@gmail.com>
2020-01-26 00:15:35 -0800

    Added a few private functions in MainActivity

--- app/src/main/java/com/example/cameragalleryapp/MainActivity.java
+++ app/src/main/java/com/example/cameragalleryapp/MainActivity.java
@@ -24,10 +24,14 @@ public class MainActivity extends AppCompatActivity {
     static final int REQUEST_IMAGE_CAPTURE = 1;
     String mCurrentPhotoPath;
 
+    static int imageCount = 0; // create an init the count to zero unless there already exists an image with higher number
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        initImageCount(); // Determine the image count
     }
 
 
@@ -66,11 +70,13 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
-    //
+    // This will create an image file as well as the caption file
     public File createImageFile() throws IOException {
         // Create an image file name
         String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
-        String imageFileName = "JPEG_" + timeStamp + "_"; // add time stamp to file name
+        String imageCount_str = intToString(imageCount);
+
+        String imageFileName = "IMG" + imageCount_str + "_" +  timeStamp + "_"; // add time stamp to file name
         File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
         File image = File.createTempFile(imageFileName, ".jpg",storageDir);
         mCurrentPhotoPath = image.getAbsolutePath();
@@ -86,4 +92,33 @@ public class MainActivity extends AppCompatActivity {
             mImageView.setImageBitmap(BitmapFactory.decodeFile(mCurrentPhotoPath)); //JPEG to BITMAP (bit map has intensity at each pixel)
         }
     }
+
+
+
+    // Convert the image count into a string with fixed length
+    // VB
+    static private String intToString(int number) {
+        int stringLengthDesired = 5; // number of characters in the string
+
+        String number_str = Integer.toString(number);
+        int numberOfZeros = stringLengthDesired - number_str.length();
+
+        // Add zeros in front of the number to match the desired length of string
+        for (int i = 0; i<numberOfZeros; i++) {
+            number_str = "0" + number_str;
+        }
+
+        return number_str;
+    }
+
+    // Upon startup, determine the image count
+    // VB
+    static private void initImageCount() {
+        // TODO: Read from the memory to establish such relationship
+        imageCount = 0;
+    }
+
+
+
+
 }

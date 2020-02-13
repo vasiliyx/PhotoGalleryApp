aaf352f424ee829f8e5414dded953180dac0eeed
Rabby <fazlay_rabby@hotmail.com>
2020-02-12 22:40:14 -0800

    Notes on what has been: //Automated test for location based search functional!
    Notes on any errors:
    //app will crash if "snap" is pressed within a few seconds of the app launch
    //this is because the gps location takes a few seconds to update, there is a reference to a
    //null pointer in the mean time, this issue will be resolved late by either redirecting
    //pointer or handling the null pointer

--- app/src/androidTest/java/com/example/cameragalleryapp/UILocationTest.java
+++ app/src/androidTest/java/com/example/cameragalleryapp/UILocationTest.java
@@ -1,17 +1,36 @@
 package com.example.cameragalleryapp;
 
-import org.junit.After;
-import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
 
-import static org.junit.Assert.*;
+import androidx.test.espresso.Espresso;
+import androidx.test.espresso.action.EspressoKey;
+import androidx.test.rule.ActivityTestRule;
+import androidx.test.runner.AndroidJUnit4;
 
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.action.ViewActions.closeSoftKeyboard;
+import static androidx.test.espresso.action.ViewActions.pressImeActionButton;
+import static androidx.test.espresso.action.ViewActions.typeText;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
+
+// Take a photo and labels it with a caption and then searches for that caption
+@RunWith(AndroidJUnit4.class)
 public class UILocationTest {
-
-    @Before
-    public void setUp() throws Exception {
-    }
-
-    @After
-    public void tearDown() throws Exception {
+    @Rule
+    public ActivityTestRule<MainActivity> mActivityRule = new ActivityTestRule<>(MainActivity.class);
+    @Test
+    public void ensureCaptionSearchWorks() {
+        onView(withId(R.id.filterPhotoButton)).perform(click()); // first press the filter button
+        onView(withId(R.id.topLeftLatEditText)).perform(typeText("52")); //type in the coordinates for BC
+        onView(withId(R.id.topLeftLongEditText)).perform(typeText("-130")); //type in the coordinates for BC
+        Espresso.closeSoftKeyboard();
+        onView(withId(R.id.bottomRightLatEditText)).perform(typeText("40")); //type in the coordinates for BC
+        onView(withId(R.id.bottomRightLongEditText)).perform(typeText("-110"),closeSoftKeyboard()); //type in the coordinates for BC
+        onView(withId(R.id.searchLocationButton)).perform(click()); // press enter
     }
 }
